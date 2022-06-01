6c2afdc9d0c85097f07c926e40e42324cda9425f
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-06-02 08:42:55 +0900

    Initial Commit

---
+++ Shared/InteractiveCommentsSectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveCommentsSectionApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/06/02.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveCommentsSectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
